3899c25ccc1911ddf394239ed04737fecbe26d4e
swlws <979623074@qq.com>
2025-08-07 09:45:30 +0800

    bash

--- cert-mgr/upload.sh
+++ cert-mgr/upload.sh
@@ -3,9 +3,32 @@
 # === 配置区域 ===
 # 远程服务器信息
 REMOTE_USER="root"
-REMOTE_HOST="your.server.com"
 LOCAL_CERT_DIR="$PWD/cert"
 REMOTE_DIR="$pwd/root/e-recycle-env/cert"
+
+# 定义服务器选项数组
+options=(
+    "dev - root@dev.swlws.site"
+    "prod - root@swlws.site"
+)
+
+# 显示服务器列表
+echo "可用的服务器列表（共 ${#options[@]} 个）："
+PS3="请选择目标服务器 [1-${#options[@]}]: "
+
+# 使用 select 进行选择
+select choice in "${options[@]}"; do
+    if [ -n "$choice" ]; then
+        # 解析选择的服务器信息
+        SERVER_KEY=$(echo $choice | cut -d'-' -f1 | xargs)
+        REMOTE_HOST=$(echo $choice | cut -d'-' -f2 | xargs)
+        break
+    else
+        echo "错误：无效的选择，请输入 1 到 ${#options[@]} 之间的数字"
+    fi
+done
+
+echo "已选择服务器: $REMOTE_HOST ($SERVER_KEY)"
 
 # === 步骤 1: 上传文件 ===
 echo "开始上传证书文件..."
